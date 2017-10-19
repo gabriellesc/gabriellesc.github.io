7e6083cdc2b199689cb364dcd8670f9a332a0582
gabriellesc <gabrielle.singhcadieux@mail.utoronto.ca>
2017-10-19 14:06:17 -0400

    embedded jsfuck presentation

--- js/template.js
+++ js/template.js
@@ -2,7 +2,7 @@
 
 let nav = document.createElement('div');
 nav.innerHTML = `<ul class="nav nav-tabs">
-    <li role="presentation"><a href="../index.html">Home</a></li>
+    <li role="presentation"><a href="../index.html"><b>Home</b></a></li>
     <li role="presentation" id="robotGrid-nav">
     <a href="../robotGrid/index.html">Robot Navigation</a>
     </li>
@@ -23,6 +23,9 @@ nav.innerHTML = `<ul class="nav nav-tabs">
     </li>
     <li role="presentation" id="piet-nav">
     <a href="../piet/index.html">Piet IDE</a>
+    </li>
+    <li role="presentation" id="jsfuck-nav">
+    <a href="../jsfuck/pres.html">JSFuck Presentation</a>
     </li>
     </ul>`;
 
