24a43e9e0f1c31205fa4dd059e449ca289a2c3e6
gabriellesc <gabrielle.singhcadieux@mail.utoronto.ca>
2017-12-03 15:16:35 -0500

    added mazily

--- js/template.js
+++ js/template.js
@@ -2,38 +2,41 @@
 
 let nav = document.createElement('div');
 nav.innerHTML = `<ul class="nav nav-tabs">
-    <li role="presentation"><a href="../index.html"><b>Home</b></a></li>
-    <li role="presentation" id="robotGrid-nav">
-    <a href="../robotGrid/index.html">Robot Navigation</a>
-    </li>
-    <li role="presentation" id="ticTacToe-nav">
-    <a href="../ticTacToe/index.html">Tic Tac Toe</a>
-    </li>
-    <li role="presentation" id="GOL-nav">
-    <a href="../GOL/index.html">Game of Life</a>
-    </li>
-    <li role="presentation" id="spaceInvaders-nav">
-    <a href="../spaceInvaders/index.html">Space Invaders</a>
-    </li>
-    <li role="presentation" id="wordplay-nav">
-    <a href="../wordplay/index.html">Wordplay</a>
-    </li>
-    <li role="presentation" id="logos-nav">
-    <a href="../logos/index.html">RC Logos</a>
-    </li>
-    <li role="presentation" id="piet-nav">
-    <a href="../piet/index.html">Piet IDE</a>
-    </li>
-    <li role="presentation" id="jsfuck-nav">
-    <a href="../jsfuck/pres.html">JSFuck Presentation</a>
-    </li>
+      <li role="presentation"><a href="../index.html"><b>Home</b></a></li>
+      <li role="presentation" id="GOL-nav">
+        <a href="../GOL/index.html">Game of Life</a>
+      </li>
+      <li role="presentation" id="ticTacToe-nav">
+        <a href="../ticTacToe/index.html">Tic Tac Toe</a>
+      </li>
+      <li role="presentation" id="mazily-nav">
+        <a href="../mazily/index.html">Mazily</a>
+      </li>
+      <li role="presentation" id="robotGrid-nav">
+        <a href="../robotGrid/index.html">Robot Navigation</a>
+      </li>
+      <!--li role="presentation" id="spaceInvaders-nav">
+        <a href="../spaceInvaders/index.html">Space Invaders</a>
+      </li-->
+      <li role="presentation" id="piet-nav">
+        <a href="../piet/index.html">Piet IDE</a>
+      </li>
+      <li role="presentation" id="wordplay-nav">
+        <a href="../wordplay/index.html">Wordplay</a>
+      </li>
+      <li role="presentation" id="logos-nav">
+        <a href="../logos/index.html">RC Logos</a>
+      </li>
+      <li role="presentation" id="jsfuck-nav">
+        <a href="../jsfuck/pres.html">JSFuck Presentation</a>
+      </li>
     </ul>`;
 
 let footer = document.createElement('div');
 footer.innerHTML = `<footer class="footer">
-    <div class="container col-xs-12">
-    <span class="text-muted">Gabrielle Singh Cadieux, 2017.</span>
-    </div>
+      <div class="container col-xs-12">
+        <span class="text-muted">Gabrielle Singh Cadieux, 2017.</span>
+      </div>
     </footer>`;
 
 document.body.insertBefore(footer, document.body.firstChild);
