193372d5175209bdb66465e454575fff782c77ec
gabriellesc <gabrielle.singhcadieux@mail.utoronto.ca>
2017-12-26 00:43:17 -0500

    removing files moved to submodule

--- robotGrid/js/actions/index.js
+++
@@ -1,9 +0,0 @@
-function resize(height, width) {
-    return { type: 'RESIZE', payload: { height, width } };
-}
-
-function changeDuration(duration) {
-    return { type: 'CHANGE_DURATION', payload: { duration } };
-}
-
-export { resize, changeDuration };
